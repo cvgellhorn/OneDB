53adb7f628eeabc6c6c038fab6fabe3afdadea0f
cvgellhorn <gellborn@gmail.com>
2014-05-09 23:46:29 +0200

    setUp and tearDown replaced

--- tests/OneDBTest.php
+++ tests/OneDBTest.php
@@ -14,7 +14,7 @@ class OneDBTest extends PHPUnit_Framework_TestCase
 	 *
 	 * @var OneDB
 	 */
-	private $_db;
+	private static $_db;
 
 	/**
 	 * Test DB table
@@ -23,47 +23,47 @@ class OneDBTest extends PHPUnit_Framework_TestCase
 	 */
 	private $_table = 'data';
 
-	public function setUp()
+	public function setUpBeforeClass()
 	{
-		$this->_db = OneDB::getInstance(array(
+		self::$_db = OneDB::getInstance(array(
 			'database'  => $GLOBALS['db_database'],
 			'user'      => $GLOBALS['db_user'],
 			'password'  => $GLOBALS['db_password']
 		));
 
 		// Init test data and also perform test on query method
-		$this->_db->query(
+		self::$_db->query(
 			'CREATE TABLE IF NOT EXISTS '
-			. $this->_db->btick($this->_table). ' ('
+			. self::$_db->btick($this->_table). ' ('
 			. 'id INT(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,'
 			. 'name VARCHAR(50) NOT NULL'
 			. ') ENGINE = InnoDB'
 		);
 	}
 
-	public function tearDown()
+	public function tearDownAfterClass()
 	{
-		$this->_db->drop($this->_table);
+		self::$_db->drop($this->_table);
 	}
 
 	public function testGetPDO()
 	{
-		$this->assertInstanceOf('PDO', $this->_db->getPDO());
+		$this->assertInstanceOf('PDO', self::$_db->getPDO());
 	}
 
 	public function testQuote()
 	{
-		$this->assertEquals("'test'", $this->_db->quote('test'));
+		$this->assertEquals("'test'", self::$_db->quote('test'));
 	}
 
 	public function testBtick()
 	{
-		$this->assertEquals("`test`", $this->_db->btick('test'));
+		$this->assertEquals("`test`", self::$_db->btick('test'));
 	}
 
 	public function testInsert()
 	{
-		$id = $this->_db->insert($this->_table, array(
+		$id = self::$_db->insert($this->_table, array(
 			'name' => 'John Doe'
 		));
 
@@ -74,15 +74,15 @@ class OneDBTest extends PHPUnit_Framework_TestCase
 	{
 		$name = 'Steve Jobs';
 
-		$this->_db->update(
+		self::$_db->update(
 			$this->_table,
 			array('name' => $name),
 			array('id = ?' => 1)
 		);
 
-		$result = $this->_db->fetchRow(
-			'SELECT * FROM ' . $this->_db->btick($this->_table)
-			. ' WHERE ' . $this->_db->btick('id') . ' = 1'
+		$result = self::$_db->fetchRow(
+			'SELECT * FROM ' . self::$_db->btick($this->_table)
+			. ' WHERE ' . self::$_db->btick('id') . ' = 1'
 		);
 
 		$this->assertEquals($name, $result['name']);
@@ -90,8 +90,8 @@ class OneDBTest extends PHPUnit_Framework_TestCase
 
 	public function testFetchAll()
 	{
-		$result = $this->_db->fetchAll(
-			'SELECT * FROM ' . $this->_db->btick($this->_table)
+		$result = self::$_db->fetchAll(
+			'SELECT * FROM ' . self::$_db->btick($this->_table)
 		);
 
 		$this->assertTrue(is_array($result[0]));
